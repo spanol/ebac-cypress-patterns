75337ac201de6bea0a972287cc5607e23e3acc20
spanol <viniciusspanol@gmail.com>
2025-07-10 03:29:57 -0300

    AppActions Exercise

--- cypress/e2e/AppActions.cy.js
+++ cypress/e2e/AppActions.cy.js
@@ -1,5 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const user = require("../fixtures/user.json");
+const address = require("../fixtures/address.json");
+
+describe("Teste de checkout", () => {
+  beforeEach(() => {
+    cy.setCookie("ebacStoreVersion", "v2", {
+      domain: "lojaebac.ebaconline.art.br",
+    });
+    cy.visit("/");
+  });
+
+  it("Deve criar um novo endereço e efetuar uma compra", () => {
+    cy.command("login", user.email, user.password);
+    cy.command("openCart");
+    cy.command(
+      "addAddress",
+      address.name,
+      address.phone,
+      address.address,
+      address.city,
+      address.state,
+      address.zipCode
+    );
+    cy.command("addRandomProductToCart");
+  });
+
+  it("Deve efetuar uma compra com um endereço existente", () => {
+    cy.command("login", user.email, user.password);
+    cy.command("openCart");
+    cy.command("addRandomProductToCart");
+  });
+});
