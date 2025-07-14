13c2576714ce23ea7e4d3d515ac1f301700318db
spanol <viniciusspanol@gmail.com>
2025-07-14 15:45:57 -0300

    Alterações na utlização de comandos cypress, adição de fixture para categorias e produtos, automação e implementação do intercept

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,35 +11,45 @@
 //
 // -- This is a parent command --
 Cypress.Commands.add("login", (email, password) => {
-  cy.command("openMenu", "Account");
+  cy.openMenu("Account");
   cy.get("[data-testid='email']").type(email);
   cy.get("[data-testid='password']").type(password, { log: false });
   cy.get('[data-testid="btnLogin"]').click();
 });
 
 Cypress.Commands.add("addRandomProductToCart", () => {
-  cy.command("openMenu", "Browse");
-  const randomColumn = Math.floor(Math.random() * 4) + 1;
-  const randomIndex = Math.floor(Math.random() * 3);
+  cy.openMenu("Browse");
+
+  const getRandomBetween = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const randomColumn = getRandomBetween(1, 2);
+  const randomIndex = getRandomBetween(1, 2);
+
   cy.get(
-    `[style="padding: 8px;"] > :nth-child(${randomColumn}) > .r-18u37iz > :nth-child(${
-      randomIndex + 1
-    }) > [data-testid="productDetails"]`
+    `[style="padding: 8px;"] > :nth-child(${randomColumn}) > .r-18u37iz > :nth-child(${randomIndex}) > [data-testid="productDetails"]`
   ).click();
   cy.get('[data-testid="addToCart"]').click();
 
   cy.get(
     '[style="z-index: 0; display: flex;"] > [style="background-color: rgb(242, 242, 242);"] > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > [style="background-color: rgb(242, 242, 242); display: flex;"] > :nth-child(1) > :nth-child(1) > .r-13awgt0 > :nth-child(3) > [data-testid="selectAddressOrContinueToPayment"]'
   ).click();
-  //   cy.get('[data-testid="selectAddressOrContinueToPayment"]').first().click();
 
+  cy.get('[data-testid="completeCheckout"]').first().click();
+});
+
+Cypress.Commands.add("addProductToCart", () => {
+  cy.get(
+    '[style="margin: 5px 7px; border-color: rgb(128, 128, 128); border-width: 0.5px; border-radius: 15px; width: 445px;"] > [data-testid="productDetails"]'
+  ).click();
+  cy.get("[data-testid='addToCart']").click();
+  cy.get('[data-testid="selectAddressOrContinueToPayment"]').click();
   cy.get('[data-testid="completeCheckout"]').first().click();
 });
 
 Cypress.Commands.add(
   "addAddress",
   (name, phone, address, city, state, zipCode) => {
-    // if button is not visible, user already has an address
     cy.get('[data-testid="addNewAddress"]').click();
     cy.get(".r-1d7mnkm > :nth-child(1) > .css-175oi2r > .css-11aywtz").type(
       name
--- cypress/support/pages/home.page.js
+++ cypress/support/pages/home.page.js
@@ -2,4 +2,20 @@ export const homePage = {
   openMenu(menu) {
     return cy.get(`[href="/Tab/${menu}"]`).click();
   },
+
+  openSearchProduct() {
+    cy.get('[data-testid="search-products"]').click();
+  },
+
+  openCategoriesFilter() {
+    return cy.get('[data-testid="Category"]').click();
+  },
+
+  categories() {
+    return cy.get('[data-testid^="search-category-"]');
+  },
+
+  selectCategory(categoryName = "Roupas") {
+    return cy.get(`[data-testid="search-category-${categoryName}"]`).click();
+  },
 };
